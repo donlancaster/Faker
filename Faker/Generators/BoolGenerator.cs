f24464c1a18a65c9eeb51cedfb937a61f92b52a7
thinkpad <donlancaster228@yandex.ru>
2021-10-31 16:45:26 +0300

    added  plugin loader, faker class

--- Faker/Generators/BoolGenerator.cs
+++ Faker/Generators/BoolGenerator.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FakerLib.Interfaces;
-
 namespace FakerLib.Generators
 {
-    public class DoubleGenerator : IGenerator
+
+    public class BoolGenerator : IGenerator
     {
-        public Type GeneratorType => typeof(Double);
+        public Type GeneratorType => typeof(bool);
 
         public object Create()
         {
-            return new Random().NextDouble();
+            return new Random().Next(0, 2) == 1;
         }
     }
 }
--- Faker/Interfaces/IFaker.cs
+++ Faker/Interfaces/IFaker.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FakerLib
+namespace FakerLib.Interfaces
 {
     public interface IFaker
     {
         object Create(Type type);
     }
+
 }
